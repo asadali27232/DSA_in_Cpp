0c50722c6275e7300add3869d292080e8f3a09d8
Asad Ali <asadali27232@gmail.com>
2022-10-02 18:09:52 +0500

    Updated

--- LAB 3/Stack.cpp
+++ LAB 3/Stack.cpp
@@ -20,9 +20,8 @@ void Stack<T>::print() {
     if (isEmpty()) {
         cout << "Empty Stack!" << endl;
     } else {
-        cout << "Stack" << endl;
         for (int i = TOP; i >= 0; --i) {
-            cout << stack[i] << endl;
+            cout << stack[i];
         }
     }
 }
@@ -32,19 +31,18 @@ void Stack<T>::push(T element) {
     if (isFull()) {
         cout << "OverFlow Condition i.e Stack is Full." << endl;
     } else {
-        stack[++TOP] = element;
-        cout << "Element Pushed!" << endl;
+        TOP += 1;
+        stack[TOP] = element;
     }
 }
 
 template<typename T>
 T Stack<T>::pop() {
-    if (isEmpty()) {
-        cout << "UnderFlow Condition i.e Stack is Empty." << endl;
-    } else {
-        cout << "Element Pop!" << endl;
-        return stack[TOP--];
+    if (!isEmpty()) {
+        TOP -= 1;
+        return stack[TOP + 1];
     }
+    cout << "UnderFlow Condition i.e Stack is Empty." << endl;
 }
 
 template<typename T>
--- LAB 3/main.cpp
+++ LAB 3/main.cpp
@@ -1,12 +1,89 @@
 #include "iostream"
 #include "Stack.h"
 #include "Stack.cpp"
+#include "cstring"
 
 using namespace std;
 
-int main() {
-    Stack<int> stack(10);
-    stack.push(20);
+void baseConversion();
 
+void balancedExpression();
+
+void infixToPostfix();
+
+void evaluatePostfix();
+
+int main() {
+//    baseConversion();
+    balancedExpression();
     return 0;
+}
+
+void baseConversion() {
+    int number;
+    int base;
+
+    Stack<int> stack(100);
+
+    cout << "Enter a Decimal Number:";
+    cin >> number;
+    cout << "Enter Output Base:";
+    cin >> base;
+
+    while (number > 0) {
+        stack.push(number % base);
+        number = number / base;
+    }
+    stack.print();
+}
+
+void balancedExpression() {
+    string str;
+    cout << "Enter expression without spaces:" << endl;
+    cin >> str;
+
+    char expression[str.length() + 1];
+    strcpy(expression, str.c_str());
+
+    Stack<char> stack(str.length() + 1);
+
+    for (int i = 0; i < str.length() + 1; ++i) {
+        if (expression[i] == '[' || expression[i] == '{' || expression[i] == '(' || expression[i] == ']' ||
+            expression[i] == '}' || expression[i] == ')') {
+
+            if (expression[i] == '[' || expression[i] == '{' || expression[i] == '(') {
+                stack.push(expression[i]);
+            } else if ((expression[i] == ']' || expression[i] == '}' || expression[i] == ')') && (!stack.isEmpty())) {
+                char openingSymbol = stack.pop();
+                char closingSymbol;
+
+                if (openingSymbol == '[')
+                    closingSymbol = ']';
+                else if (openingSymbol == '{')
+                    closingSymbol = '}';
+                else
+                    closingSymbol = ')';
+
+                if (expression[i] != closingSymbol) {
+                    cout << "Expression is not in Balanced form!" << endl;
+                    return;
+                }
+            } else if ((expression[i] == ']' || expression[i] == '}' || expression[i] == ')') && (stack.isEmpty())) {
+                cout << "Expression is not in Balanced form!" << endl;
+                return;
+            }
+        }
+    }
+    if (stack.isEmpty())
+        cout << "Expression is in Balanced form." << endl;
+    else
+        cout << "Expression is not in Balanced form!" << endl;
+}
+
+void infixToPostfix() {
+
+}
+
+void evaluatePostfix() {
+
 }
